e7a7af7966d33727453f35b479f9b9c4d09c3fe1
Elsa Dwi Agresty <112572323+Elsadwiagresty07@users.noreply.github.com>
2022-09-22 00:59:10 +0700

    Add files via upload

---
+++ belajar.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	int a;
+	float b;
+	string c;
+	char d;
+
+	cin >> a >> b >> c >> d;
+
+	cout << "integer \t :" << a << endl;
+	cout << "float \t :" << b << endl;
+	cout << "string \t :" << c << endl;
+	cout << "char \t :" << d << endl;
+
+	return 0;
+}
